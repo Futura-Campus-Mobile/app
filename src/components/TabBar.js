30166456299667a90d274c03b04fca7dfe928593
Lucas Lima <llbarbosasc@gmail.com>
2020-01-21 23:39:52 -0400

    feat: Adicionando animaçoes na TabBar

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { StyleSheet, View, TouchableOpacity, Text, Animated } from 'react-native'
 
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50
     },
-    actionBar: ({ show }) => ({
+    actionBar: {
         paddingHorizontal: 20,
         flexDirection: "row",
         justifyContent: 'space-around',
@@ -44,55 +44,63 @@ const styles = StyleSheet.create({
         borderTopStartRadius: 30,
         borderTopEndRadius: 30,
         backgroundColor: '#dcdcdc',
-        height: 90,
-        bottom: show ? 0 : -100,
-    })
+        height: 90
+    }
 })
 
-const BarButton = ({ iconName, text, size = 25, selected = false, onPress }) => (
+const BarButton = ({ iconName, text, size = 25, selected = false, showText = true, onPress }) => (
     <TouchableOpacity style={styles.barButton} onPress={onPress}>
         <Icon name={iconName} color={selected ? '#000' : '#b7b7b7'} size={size} />
-        {!selected && <Text 
-            style={{ 
-                fontSize: size / 2.2, 
+        {(!selected && showText) && <Text
+            style={{
+                fontSize: size / 2.2,
                 fontFamily: 'raleway-regular',
-                color: '#b7b7b7' 
+                color: '#b7b7b7'
             }}
         >{text}</Text>}
     </TouchableOpacity>
 )
 
-const ActionBar = ({ navigation, show, toggle }) => (
-    <View style={styles.actionBar({ show })}>
-        <BarButton
-            iconName="kitchen"
-            text="Device"
-            size={30}
-            onPress={() => { toggle(), navigation.navigate('DeviceEdit') }}
-        />
-        <View style={{ height: 90, backgroundColor: '#d2d2d2', width: 1.2 }} />
-        <BarButton
-            iconName="power"
-            text="Plug"
-            size={30}
-            onPress={() => { toggle(), navigation.navigate('PlugEdit') }}
-        />
-    </View>
-)
+const ActionBar = ({ navigation, show, toggle }) => {
+    const [bottom] = useState(new Animated.Value(-100))
+
+    useEffect(() => {
+        Animated.spring(bottom, { toValue: show ? 0 : -100}).start()
+    }, [show])
+
+    return (
+        <Animated.View style={{...styles.actionBar, bottom }}>
+            <BarButton
+                iconName="kitchen"
+                text="Device"
+                size={30}
+                onPress={() => { toggle(), navigation.navigate('DeviceEdit') }}
+            />
+            <View style={{ height: 90, backgroundColor: '#d2d2d2', width: 1.2 }} />
+            <BarButton
+                iconName="power"
+                text="Plug"
+                size={30}
+                onPress={() => { toggle(), navigation.navigate('PlugEdit') }}
+            />
+        </Animated.View>
+    )
+}
 
 export default class TabBar extends React.Component {
     state = {
         showActionBar: false,
+        barHeight: new Animated.Value(90),
         toggleActionBar: () => this.setState({ showActionBar: !this.state.showActionBar }),
         isActiveRoute: name => this.props.navigation.state.routes[this.props.navigation.state.index].routeName === name,
         mode: {
-            goBack: { 
+            goBack: {
                 fabIconName: 'keyboard-arrow-left',
                 buttomColor: '#000',
                 iconColor: '#fff',
                 onPress: () => this.props.navigation.navigate('Home')
             },
-            addDevice: { 
+            addDevice: {
                 fabIconName: 'add',
                 buttomColor: '#9510AC',
                 onPress: () => this.state.toggleActionBar()
@@ -118,18 +126,24 @@ export default class TabBar extends React.Component {
 
     render() {
         const { navigation } = this.props
-        const { toggleActionBar, getCurrentMode, isActiveRoute, showActionBar, mode } = this.state
+        const {
+            toggleActionBar, barHeight, getCurrentMode,
+            isActiveRoute, showActionBar, mode
+        } = this.state
 
-        if(showActionBar && getCurrentMode() !== mode.closeActionBar)
+        Animated.spring(this.state.barHeight, { toValue: this.state.isActiveRoute('Home') ? 90 : 50 }).start()
+
+        if (showActionBar && getCurrentMode() !== mode.closeActionBar)
             toggleActionBar()
 
         return (
             <View style={styles.container}>
                 <ActionBar navigation={navigation} show={showActionBar} toggle={toggleActionBar} />
-                <View style={styles.tabBar}>
+                <Animated.View style={{ ...styles.tabBar, height: barHeight }}>
                     <BarButton
                         iconName="equalizer"
                         text="Overview"
+                        showText={isActiveRoute('Home')}
                         selected={isActiveRoute('Overview')}
                         onPress={() => navigation.navigate('Overview')}
                     />
@@ -139,10 +153,11 @@ export default class TabBar extends React.Component {
                     <BarButton
                         iconName="person-outline"
                         text="Profile"
+                        showText={isActiveRoute('Home')}
                         selected={isActiveRoute('Profile')}
                         onPress={() => navigation.navigate('Profile')}
                     />
-                </View>
+                </Animated.View>
             </View>
         )
     }
